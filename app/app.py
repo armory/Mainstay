4394870ec0c1e40c0c267251453a6908a1d6d062
cremerfc <fernandocremer@Fernandos-MacBook-Pro.local>
2020-01-07 14:01:25 -0500

    changed app to use templates

--- app/app.py
+++ app/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return "This is the root context!"
+def index():
+    return render_template('index.html')
 
 @app.route('/test')
 def test():
